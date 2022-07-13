f2d0e1465b0535e96a2f40e2cebe0b0a26eb06c5
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-07-13 23:21:34 +0200

    ajout de la tierlist en 2

--- src/app/tierlist/tierlist.component.ts
+++ src/app/tierlist/tierlist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Subscription } from 'rxjs';
 import { Prof } from '../models/Prof.model';
 import { ListService } from '../services/list.service';
@@ -20,7 +20,13 @@ export class TierlistComponent implements OnInit {
   //   { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
   // ];
 
-  profs!: Prof[]
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  profs!: Prof[] | any[]
+  profs1: string[] = []
+  profs2: string[] = []
   profsSubscription!: Subscription
 
   constructor(private listService: ListService, private router: Router, private route: ActivatedRoute) { }
@@ -36,14 +42,36 @@ export class TierlistComponent implements OnInit {
     this.profsSubscription = this.listService.profsSubject.subscribe(
       (profs: Prof[]) => {
         this.profs = profs
+
+        for (let prof = 0; prof <= this.profs.length - 1; prof++) {
+          if (prof < 10) {
+            console.log(this.profs[prof].name);
+
+            this.profs1.push(this.profs[prof].name)
+          } else {
+            console.log(this.profs[prof].name);
+            this.profs2.push(this.profs[prof].name)
+          }
+        }
       }
     );
-    this.listService.getProfs()
-    this.listService.emitProfs()
+    this.listService.getProfs();
+    this.listService.emitProfs();
+
+
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.profs, event.previousIndex, event.currentIndex);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
   //! onSave(){} pour plus tard je pense
