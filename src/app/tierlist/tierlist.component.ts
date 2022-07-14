04ba64b68e53cb5d0d6ca90032055b4f53d65b9a
BURNEL Justin <justin.burnel3@etu.univ-lorraine.fr>
2022-07-14 02:39:09 +0200

    ajout de la tierlist scindé correctement

--- src/app/tierlist/tierlist.component.ts
+++ src/app/tierlist/tierlist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDragEnter, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Subscription } from 'rxjs';
 import { Prof } from '../models/Prof.model';
 import { ListService } from '../services/list.service';
@@ -12,21 +12,7 @@ import { getAuth } from 'firebase/auth';
   styleUrls: ['./tierlist.component.scss']
 })
 export class TierlistComponent implements OnInit {
-  // users: { name: string, title: string }[] = [
-  //   { name: 'Carla Espinosa', title: 'Nurse' },
-  //   { name: 'Bob Kelso', title: 'Doctor of Medicine' },
-  //   { name: 'Janitor', title: 'Janitor' },
-  //   { name: 'Perry Cox', title: 'Doctor of Medicine' },
-  //   { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
-  // ];
-
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
   profs!: Prof[] | any[]
-  profs1: string[] = []
-  profs2: string[] = []
   profsSubscription!: Subscription
 
   constructor(private listService: ListService, private router: Router, private route: ActivatedRoute) { }
@@ -42,17 +28,6 @@ export class TierlistComponent implements OnInit {
     this.profsSubscription = this.listService.profsSubject.subscribe(
       (profs: Prof[]) => {
         this.profs = profs
-
-        for (let prof = 0; prof <= this.profs.length - 1; prof++) {
-          if (prof < 10) {
-            console.log(this.profs[prof].name);
-
-            this.profs1.push(this.profs[prof].name)
-          } else {
-            console.log(this.profs[prof].name);
-            this.profs2.push(this.profs[prof].name)
-          }
-        }
       }
     );
     this.listService.getProfs();
@@ -62,26 +37,30 @@ export class TierlistComponent implements OnInit {
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
+    moveItemInArray(this.profs, event.previousIndex, event.currentIndex);
   }
 
   //! onSave(){} pour plus tard je pense
   test() {
     console.log('test');
     console.log(this.profs);
-
   }
 
   wichPic(name: string) {
     return `../../assets/PhotoProfs/${name.split('.').join('')}.jpg`;
+  }
+
+  dragEntered(event: CdkDragEnter<number>) {
+    const drag = event.item;
+    const dropList = event.container;
+    const dragIndex = drag.data;
+    const dropIndex = dropList.data;
+
+    // const phContainer = dropList.element.nativeElement;
+    // const phElement = phContainer.querySelector('.cdk-drag-placeholder');
+    // phContainer.removeChild(phElement!);
+    // phContainer.parentElement!.insertBefore(phElement!, phContainer);
+
+    moveItemInArray(this.profs, dragIndex, dropIndex);
   }
 }
