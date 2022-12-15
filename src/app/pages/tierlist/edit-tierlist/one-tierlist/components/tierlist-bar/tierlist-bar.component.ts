69d60dc79e293c19255300440fc0a347a61e191e
justin burnel <justin.burnel@gmail.com>
2022-12-15 21:01:11 +0100

    ajout d'animation

--- src/app/pages/tierlist/edit-tierlist/one-tierlist/components/tierlist-bar/tierlist-bar.component.ts
+++ src/app/pages/tierlist/edit-tierlist/one-tierlist/components/tierlist-bar/tierlist-bar.component.ts
@@ -1,28 +1,61 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Tierlist } from 'src/app/interface/tierlist.interface';
+import {Component, Input, OnInit} from '@angular/core';
+import {Tierlist} from 'src/app/interface/tierlist.interface';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {animate, animateChild, group, keyframes, query, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-tierlist-bar',
   templateUrl: './tierlist-bar.component.html',
-  styleUrls: ['./tierlist-bar.component.scss']
+  styleUrls: ['./tierlist-bar.component.scss'],
+  animations: [
+    trigger('grow', [
+      state('false', style({height : '50px'})),
+      state('true', style({height : '70px'})),
+      transition('false => true', [
+          style({height: '50px'}),
+          animate('1s ease', style({height : '70px'})),
+        group([query('@errorAnimation', animateChild())])
+        ]),
+      transition('true => false', [
+          style({height: '70px'}),
+          animate('1s ease', style({height : '50px'})),
+        group([query('@errorAnimation', animateChild())])
+        ]),
+    ]),
+
+    //? child
+    trigger('errorAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('2s', keyframes([
+          style({ opacity: 0 , offset : 0.5}),
+          style({ opacity: 1 , offset : 0.7})
+        ]))
+      ]),
+      transition(':leave', [
+        animate('0.150s', style({ opacity: 0 }),)
+      ]),
+    ]),
+  ]
 })
 export class TierlistBarComponent implements OnInit {
-  @Input() tierlist: Tierlist = { author: ' sdf', description: ' sdf', isPublic: false, items: [], name: ' sdf' }
+  @Input() tierlist: Tierlist = {author: ' sdf', description: ' sdf', isPublic: false, items: [], name: ' sdf'}
   @Input() isPublic!: boolean
   editTitle: boolean = false
-
-  itemName : string = ''
-  itemUrl : string = ''
+  itemName: string = ''
+  itemUrl: string = ''
 
   itemsForm!: FormGroup
 
-  constructor() { }
+  haveError : string = 'false'
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.itemsForm = new FormGroup({
-      name : new FormControl(this.itemName, [Validators.required]),
-      url : new FormControl(this.itemUrl)
+      name: new FormControl(this.itemName, [Validators.required]),
+      url: new FormControl(this.itemUrl)
     })
   }
 
@@ -31,8 +64,14 @@ export class TierlistBarComponent implements OnInit {
     console.log(this.isPublic)
   }
 
-  get name() { return this.itemsForm.get('name')}
-  get url() { return this.itemsForm.get('url')}
+
+  get name() {
+    return this.itemsForm.get('name')
+  }
+
+  get url() {
+    return this.itemsForm.get('url')
+  }
 
   toggle() {
     this.isPublic = !this.isPublic;
@@ -48,5 +87,12 @@ export class TierlistBarComponent implements OnInit {
     const name = this.itemsForm.get('name')?.value
     const url = this.itemsForm.get('url')?.value
     console.log(name, url)
+  }
+
+  onContentChange(){
+    if(this.name?.errors?.['required']){
+      this.haveError = "true"
+    }else
+      this.haveError = "false"
   }
 }
